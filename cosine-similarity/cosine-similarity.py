1bc4adaaa7e628552fbbe8e87cc8409ca96a99b9
Toprak2 <muhammedalitoprak1@gmail.com>
2026-08-28 01:06:32 +0300

    Solve cosine-similarity

--- cosine-similarity/cosine-similarity.py
+++ cosine-similarity/cosine-similarity.py
@@ -4,23 +4,13 @@ def cosine_similarity(a: list, b: list) -> float:
     """
     Returns the cosine similarity as a Python float.
     """
-    norm_a = 0.0
-    norm_b = 0.0
-    
-    dot_product=0.0
-    
-    for num1, num2 in zip(a,b):
-        dot_product += num1 * num2
+    a = np.asarray(a, dtype=float)
+    b = np.asarray(b, dtype=float)
 
-        norm_a += num1**2
-        norm_b += num2**2
-
-    norm_a = np.sqrt(norm_a)
-    norm_b = np.sqrt(norm_b)
+    norm_a = np.linalg.norm(a)
+    norm_b = np.linalg.norm(b)
 
     if norm_a == 0.0 or norm_b == 0.0:
         return 0.0
-    
-    return float(dot_product/(norm_a*norm_b))
-    
-    pass
+
+    return float(np.dot(a,b)/(norm_a * norm_b))
