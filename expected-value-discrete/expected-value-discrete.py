86ba630db70f98dd90986deb4ad3f981b6c982ca
Toprak2 <muhammedalitoprak1@gmail.com>
2026-09-01 01:37:04 +0300

    Solve expected-value-discrete

--- expected-value-discrete/expected-value-discrete.py
+++ expected-value-discrete/expected-value-discrete.py
@@ -7,6 +7,8 @@ def expected_value_discrete(x: list, p: list) -> float:
     x_arr = np.asarray(x)
     p_arr = np.asarray(p)
 
-    sum = (x_arr * p_arr).sum()
+    ## This is just dot product of x and p
+    #sum = (x_arr * p_arr).sum()
+    #return float(sum)
 
-    return float(sum)
+    return float(np.dot(x,p))
