78c26a87ae0e14263aac8c806b9fad5d51c6084a
Toprak2 <muhammedalitoprak1@gmail.com>
2026-09-02 00:46:49 +0300

    Solve bernoulli-pmf

--- bernoulli-pmf/bernoulli-pmf.py
+++ bernoulli-pmf/bernoulli-pmf.py
@@ -4,13 +4,8 @@ def bernoulli_pmf_and_moments(x: list, p: float) -> dict:
     """
     Returns a dictionary with pmf, mean, and variance.
     """
-    arr = np.asarray(x, dtype=float)
+    x = np.asarray(x, dtype=float)
 
-    for i in range(len(arr)):
-        if arr[i] == 0:
-            arr[i] = 1-p
-        else:
-            arr[i] = p
-    
+    pmf = np.where(x == 1, p, 1.0 - p).astype(float)
     variance = p*(1-p)
-    return { "pmf":arr,"mean":float(p),"variance":float(variance)}
+    return { "pmf":pmf,"mean":float(p),"variance":float(variance)}
